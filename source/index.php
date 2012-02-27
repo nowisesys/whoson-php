9d2553514b4cb69e5bf1751a8fc97ffc3f4fed43
Anders Lövgren <andlov@nowise.se>
2012-02-27 19:33:30 +0000

    The public page exposing the web service. Uses a WebService callback object to handle WSDL request and SOAP request handling.

--- source/index.php
+++ source/index.php
@@ -1,3 +1,26 @@
 <?php
 
+// Copyright (C) 2012 Computing Department BMC, 
+// Uppsala Biomedical Centre, Uppsala University.
+// 
+// File:   source/index.php
+// Author: Anders Lövgren
+// Date:   2012-02-27
+// 
+// The public page exposing the web service. All processing is handled by the
+// WebService class. 
+// 
+// This page simply detects if WSDL mode is requested, otherwise it lets the
+// service class handle the SOAP request.
+// 
+
+require_once('include/webservice.inc');
+
+if (isset($_REQUEST['wsdl'])) {
+        $service = new WebService();
+        $service->sendDescription();
+} else {
+        $service = new WebService();
+        $service->handle();
+}
 ?>
